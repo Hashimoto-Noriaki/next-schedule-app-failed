e4664b9484a081de89b8914d798b62e7dc99988b
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-27 19:11:43 +0900

    Topページ作成するための表示確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div className="bg-gray-100 min-h-screen flex items-center justify-center">
-    <h1 className="text-3xl font-bold underline text-center">Hello,Next.js</h1>
+      <h1 className="text-3xl font-bold underline text-center">Hello,Next.js</h1>
     </div>
   )
 }
