60cacda656311e12f34f51f38dd7ba1987bdec72
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-28 10:30:19 +0900

    divクラス追記

--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -2,17 +2,19 @@ import React from 'react'
 
 const TopPage = () => {
   return (
-    <header>
-        <div className="flex justify-between">
-            <p>スケジュール管理</p>
-            <nav>
-              <ul className="flex gap-7">
-              <p>利用方法</p>
-              <p>ログイン</p>
-              </ul>
-            </nav>
-        </div>
-    </header>
+    <div>
+      <header>
+          <div className="flex justify-between">
+              <p>スケジュール管理</p>
+              <nav>
+                <ul className="flex gap-7">
+                <p>利用方法</p>
+                <p>ログイン</p>
+                </ul>
+              </nav>
+          </div>
+      </header>
+    </div>
   )
 }
 
