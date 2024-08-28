5fe707d2d5a04ef6314bcecef055893002858570
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-28 15:15:52 +0900

    ログインページ作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import "../styles/destyle.css";
 import "../styles/index.css";
 import "../styles/output.css";
+import NotLoginLayout from "../components/templates/NotLoginLayout";
 
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NotLoginLayout>
+      <Component {...pageProps} />
+    </NotLoginLayout>
+  );
 }
 
 export default MyApp;
