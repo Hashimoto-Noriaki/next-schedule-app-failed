04686b2e352113a502cbedcc772a6d7bf526a213
nori.hasimo.89@gmail.com <nori.hasimo.89@gmail.com>
2024-08-28 15:56:18 +0900

    ログイン画面完成

--- src/components/templates/NotLoginLayout.tsx
+++ src/components/templates/NotLoginLayout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from "react";
+import Link from "next/link";
 
 type PropsType = {
   children: ReactNode;
@@ -9,11 +10,15 @@ export const NotLoginLayout = ({ children }: PropsType) => {
     <div className="relative">
       <header className="bg-white leading-[50px] fixed top-0 left-0 right-0">
         <div className="container mx-auto flex justify-between">
-          <p className="logo">スケジュール管理APP</p>
+          <p className="logo">
+            <Link href="/">スケジュール管理APP</Link>
+          </p>
           <nav>
             <ul className="flex gap-5 text-react-blue">
               <li>ご利用方法</li>
-              <li>ログイン</li>
+              <li>
+                <Link href="/login">ログイン</Link>
+              </li>
             </ul>
           </nav>
         </div>
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { NotLoginLayout } from "../components/templates/NotLoginLayout";
 import { PrimaryBtn } from "@/components/atoms/PrimaryBtn";
+import { useRouter } from "next/router";
 
 export const TopPage = () => {
+  const router = useRouter();
+
   return (
     <NotLoginLayout>
       <div className="text-center">
         <h1 className="text-7xl logo">スケジュール管理</h1>
         <p className="pt-[10vh] text-5xl">スケジュールを管理するアプリ</p>
         <div className="pt-[20vh]">
-          <PrimaryBtn>ログイン</PrimaryBtn>
+          <PrimaryBtn onClick={() => router.push("/login")}>
+            ログイン
+          </PrimaryBtn>
         </div>
       </div>
     </NotLoginLayout>
